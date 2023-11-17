fadc4e1f320ba5f156ec1e0d52a512c6764cf467
MickmaW <150435472+MickmaW@users.noreply.github.com>
2023-11-17 14:54:34 +0000

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,32 +1,79 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/0dECPoL80oU
+ * @see https://v0.dev/t/hjABwq6x5Um
  */
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+
 export default function Component() {
   return (
-    <div className="bg-blue-200 rounded-lg p-6 w-64 text-center shadow-lg">
-      <h1 className="text-2xl font-bold mb-2">Current Weather</h1>
-      <div className="flex items-center justify-center mb-4">
-        <svg
-          className=" h-10 w-10 text-blue-800"
-          fill="none"
-          height="24"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          width="24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
-        </svg>
-        <div className="ml-2">
-          <span className="text-4xl font-bold">72°</span>
-          <p className="text-blue-600">Cloudy</p>
-        </div>
+    <div className="space-y-8 text-blue-500 dark:text-blue-400">
+      <div className="space-y-2">
+        <h2 className="text-3xl font-bold">Guest Book</h2>
+        <p>Leave a message to let us know you were here.</p>
       </div>
-      <p className="text-blue-600">Your location</p>
+      <form className="space-y-4">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="space-y-2">
+            <Label className="text-blue-500 dark:text-blue-400" htmlFor="guest-name">
+              Name
+            </Label>
+            <Input
+              className="border-blue-500 dark:border-blue-400 focus:ring-blue-500 dark:focus:ring-blue-400"
+              id="guest-name"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="space-y-2">
+            <Label className="text-blue-500 dark:text-blue-400" htmlFor="guest-email">
+              Email
+            </Label>
+            <Input
+              className="border-blue-500 dark:border-blue-400 focus:ring-blue-500 dark:focus:ring-blue-400"
+              id="guest-email"
+              placeholder="Enter your email"
+              type="email"
+            />
+          </div>
+        </div>
+        <div className="space-y-2">
+          <Label className="text-blue-500 dark:text-blue-400" htmlFor="guest-message">
+            Message
+          </Label>
+          <Textarea
+            className="min-h-[100px] border-blue-500 dark:border-blue-400 focus:ring-blue-500 dark:focus:ring-blue-400"
+            id="guest-message"
+            placeholder="Enter your message"
+          />
+        </div>
+        <Button className="w-full bg-blue-500 hover:bg-blue-700 text-white" type="submit">
+          Add Entry
+        </Button>
+      </form>
+      <div className="space-y-4">
+        <h3 className="text-2xl font-semibold text-blue-500 dark:text-blue-400">Previous Entries</h3>
+        <ul className="divide-y divide-blue-200 dark:divide-blue-700">
+          <li className="py-4">
+            <div className="space-y-2">
+              <h4 className="font-bold text-blue-500 dark:text-blue-400">John Doe</h4>
+              <p>johndoe@example.com</p>
+              <p>Great place! I had a wonderful time.</p>
+              <p className="text-blue-400 dark:text-blue-500 text-sm">Nov 17, 2023</p>
+            </div>
+          </li>
+          <li className="py-4">
+            <div className="space-y-2">
+              <h4 className="font-bold text-blue-500 dark:text-blue-400">Jane Smith</h4>
+              <p>janesmith@example.com</p>
+              <p>I loved the atmosphere! Will definitely visit again.</p>
+              <p className="text-blue-400 dark:text-blue-500 text-sm">Nov 16, 2023</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
+
