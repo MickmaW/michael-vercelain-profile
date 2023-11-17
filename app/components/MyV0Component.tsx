d3c2beccf6b82eafe127a5df3d83e2a53a409373
MickmaW <150435472+MickmaW@users.noreply.github.com>
2023-11-17 14:49:41 +0000

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,15 +1,14 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/FoMi6q8s96o
+ * @see https://v0.dev/t/0dECPoL80oU
  */
-import { Button } from "@/components/ui/button"
-
 export default function Component() {
   return (
-    <div className="flex items-center justify-between bg-blue-100 dark:bg-blue-900 p-4 rounded-md">
-      <div className="flex items-center gap-4">
+    <div className="bg-blue-200 rounded-lg p-6 w-64 text-center shadow-lg">
+      <h1 className="text-2xl font-bold mb-2">Current Weather</h1>
+      <div className="flex items-center justify-center mb-4">
         <svg
-          className=" w-12 h-12 text-gray-500 dark:text-gray-400"
+          className=" h-10 w-10 text-blue-800"
           fill="none"
           height="24"
           stroke="currentColor"
@@ -20,22 +19,14 @@ export default function Component() {
           width="24"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M12 2v2" />
-          <path d="m4.93 4.93 1.41 1.41" />
-          <path d="M20 12h2" />
-          <path d="m19.07 4.93-1.41 1.41" />
-          <path d="M15.947 12.65a4 4 0 0 0-5.925-4.128" />
-          <path d="M13 22H7a5 5 0 1 1 4.9-6H13a3 3 0 0 1 0 6Z" />
+          <path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" />
         </svg>
-        <div>
-          <p className="text-2xl font-semibold">23°C</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">Partly Cloudy</p>
+        <div className="ml-2">
+          <span className="text-4xl font-bold">72°</span>
+          <p className="text-blue-600">Cloudy</p>
         </div>
       </div>
-      <Button className="text-sm" variant="outline">
-        Details
-      </Button>
+      <p className="text-blue-600">Your location</p>
     </div>
   )
 }
-
